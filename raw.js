878b9c49484758bb965e91db2d3820a663b77b34
DESKTOP-MHS2LG9\PCBDesign <thiendong.iuh@gmail.com>
2018-09-22 20:55:40 +0700

    Circuit Simulation

--- raw.js
+++ raw.js
@@ -25,6 +25,10 @@ var is_drapObject = false;
 var is_selecting, x_selected = false;
 var is_selecting_touch = false;
 
+var led_g=[], line_g = [], battery_g=[];
+var led_g_status = false, line_prex = 0, line_prey = 0, battery_g_status = false;
+var value_line = [];
+var index_object = 0;
 function resize_process(){
   var w = window.outerWidth;
   var h = window.outerHeight;
@@ -92,6 +96,9 @@ function windowElements_Init(){
   addbattery(x0*3/4,40,'elementsGroup',1);
   addResistor(x0/4,140,'elementsGroup',1);
   addCapacitor(x0*3/4,140,'elementsGroup',1);
+  addInductor(x0/4,240,'elementsGroup',1);
+  addPulseOut(x0*3/4,240,'elementsGroup',1);
+  addButton(x0/4,340,'elementsGroup',1);
 }
 
 function addPoint(x,y) {
@@ -182,6 +189,60 @@ function addCapacitor(x,y, group, scale) {
 
   group_.appendChild(useElem);
 }
+function addInductor(x,y, group, scale) {
+  var group_ = document.getElementById(group);
+
+  var useElem = document.createElementNS(xmlns, "use");
+
+  useElem.setAttributeNS(null, "x", x);
+  useElem.setAttributeNS(null, "y", y);
+  scale_str = 'scale('+ scale + ')';
+  useElem.setAttributeNS(null, "transform", scale_str);
+
+  //useElem.setAttributeNS(null, "fill", randomColor());
+  //useElem.setAttributeNS(null, "stroke", randomColor());
+  useElem.setAttributeNS(null, "opacity", 1);
+
+  useElem.setAttributeNS(xlinkns, "xlink:href", "#inductor_element");
+
+  group_.appendChild(useElem);
+}
+function addPulseOut(x,y, group, scale) {
+  var group_ = document.getElementById(group);
+
+  var useElem = document.createElementNS(xmlns, "use");
+
+  useElem.setAttributeNS(null, "x", x);
+  useElem.setAttributeNS(null, "y", y);
+  scale_str = 'scale('+ scale + ')';
+  useElem.setAttributeNS(null, "transform", scale_str);
+
+  //useElem.setAttributeNS(null, "fill", randomColor());
+  //useElem.setAttributeNS(null, "stroke", randomColor());
+  useElem.setAttributeNS(null, "opacity", 1);
+
+  useElem.setAttributeNS(xlinkns, "xlink:href", "#pulseOut_element");
+
+  group_.appendChild(useElem);
+}
+function addButton(x,y, group, scale) {
+  var group_ = document.getElementById(group);
+
+  var useElem = document.createElementNS(xmlns, "use");
+
+  useElem.setAttributeNS(null, "x", x);
+  useElem.setAttributeNS(null, "y", y);
+  scale_str = 'scale('+ scale + ')';
+  useElem.setAttributeNS(null, "transform", scale_str);
+
+  //useElem.setAttributeNS(null, "fill", randomColor());
+  //useElem.setAttributeNS(null, "stroke", randomColor());
+  useElem.setAttributeNS(null, "opacity", 1);
+
+  useElem.setAttributeNS(xlinkns, "xlink:href", "#button_element");
+
+  group_.appendChild(useElem);
+}
 function addLine(line_type,x,y){
   var lineGroup = document.getElementById("lineGroup");
   var useElem = document.createElementNS(xmlns, "use");
@@ -219,47 +280,306 @@ function addObject(obj, x, y, scale){
 
   useElem.setAttributeNS(xlinkns, "xlink:href", obj);
 
+  if(obj=='#led_off'){
+    led_g_status = true;
+  }
+  if(obj=='#battery'){
+    battery_g_status = true;
+  }
+
   group_.appendChild(useElem);
+  index_object = index_object + 1;
+}
+
+function addLine_G(case_){
+  switch(case_)
+  {
+    case 1:
+    {
+      var group_item = 0;
+      if(line_g.length > 0){
+        x1 = x_line_start - 50;
+        x2 = x_line_start;
+        y = y_line_start;
+        var is_choose = false, x_choose = 0;
+        //Tim diem trung voi duong co san (group co san)
+        for(var i = 0; i < line_g.length; i++){
+          if(((line_g[i]['coor']['x'] == x1)||(line_g[i]['coor']['x'] == x2))&&(line_g[i]['coor']['y'] == y)){
+            group_item = line_g[i]['group'];
+            is_choose = true;
+            if(line_g[i]['coor']['x'] == x1){
+              x_choose = 1;
+            }
+            else{
+              x_choose = 2;
+            }
+            break;
+          }
+        }
+        if(is_choose){
+          if(x_choose == 1){
+            var list_temp = {group:group_item,value:-1,coor:{x:(x_line_start), y:(y_line_start)}};
+            line_g.push(list_temp);
+          }
+          else{
+            var list_temp = {group:group_item,value:-1,coor:{x:(x_line_start - 50), y:(y_line_start)}};
+            line_g.push(list_temp);
+          }
+        }
+        else{//Truong hop diem ko thuoc cac group da tao
+          for(var i = 0; i < line_g.length; i++){
+            if(line_g[i]['group'] >= group_item){
+              group_item = group_item + 1;
+            }
+          }
+          var list_temp = {group:group_item,value:-1,coor:{x:(x_line_start - 50), y:(y_line_start)}};
+          line_g.push(list_temp);
+          var list_temp = {group:group_item,value:-1,coor:{x:(x_line_start), y:(y_line_start)}};
+          line_g.push(list_temp);
+        }
+      }
+      else {
+        var list_temp = {group:group_item,value:-1,coor:{x:(x_line_start - 50), y:(y_line_start)}};
+        line_g.push(list_temp);
+        var list_temp = {group:group_item,value:-1,coor:{x:(x_line_start), y:(y_line_start)}};
+        line_g.push(list_temp);
+      }
+      break;
+    }
+    case 2:
+    {
+      var group_item = 0;
+      if(line_g.length > 0){
+        x1 = x_line_start + 50;
+        x2 = x_line_start;
+        y = y_line_start;
+        var is_choose = false, x_choose = 0;
+        //Tim diem trung voi duong co san (group co san)
+        for(var i = 0; i < line_g.length; i++){
+          if(((line_g[i]['coor']['x'] == x1)||(line_g[i]['coor']['x'] == x2))&&(line_g[i]['coor']['y'] == y)){
+            group_item = line_g[i]['group'];
+            is_choose = true;
+            if(line_g[i]['coor']['x'] == x1){
+              x_choose = 1;
+            }
+            else{
+              x_choose = 2;
+            }
+            break;
+          }
+        }
+        if(is_choose){
+          if(x_choose == 1){
+            var list_temp = {group:group_item,value:-1,coor:{x:(x_line_start), y:(y_line_start)}};
+            line_g.push(list_temp);
+          }
+          else{
+            var list_temp = {group:group_item,value:-1,coor:{x:(x_line_start + 50), y:(y_line_start)}};
+            line_g.push(list_temp);
+          }
+        }
+        else{//Truong hop diem ko thuoc cac group da tao
+          for(var i = 0; i < line_g.length; i++){
+            if(line_g[i]['group'] >= group_item){
+              group_item = group_item + 1;
+            }
+          }
+          var list_temp = {group:group_item,value:-1,coor:{x:(x_line_start + 50), y:(y_line_start)}};
+          line_g.push(list_temp);
+          var list_temp = {group:group_item,value:-1,coor:{x:(x_line_start), y:(y_line_start)}};
+          line_g.push(list_temp);
+        }
+      }
+      else {
+        var list_temp = {group:group_item,value:-1,coor:{x:(x_line_start + 50), y:(y_line_start)}};
+        line_g.push(list_temp);
+        var list_temp = {group:group_item,value:-1,coor:{x:(x_line_start), y:(y_line_start)}};
+        line_g.push(list_temp);
+      }
+      break;
+    }
+    case 3:
+    {
+      var group_item = 0;
+      if(line_g.length > 0){
+        y1 = y_line_start - 50;
+        y2 = y_line_start;
+        x = x_line_start;
+        var is_choose = false, y_choose = 0;
+        //Tim diem trung voi duong co san (group co san)
+        for(var i = 0; i < line_g.length; i++){
+          if(((line_g[i]['coor']['y'] == y1)||(line_g[i]['coor']['y'] == y2))&&(line_g[i]['coor']['x'] == x)){
+            group_item = line_g[i]['group'];
+            is_choose = true;
+            if(line_g[i]['coor']['y'] == y1){
+              y_choose = 1;
+            }
+            else{
+              y_choose = 2;
+            }
+            break;
+          }
+        }
+        if(is_choose){
+          if(y_choose == 1){
+            var list_temp = {group:group_item,value:-1,coor:{x:(x_line_start), y:(y_line_start)}};
+            line_g.push(list_temp);
+          }
+          else{
+            var list_temp = {group:group_item,value:-1,coor:{x:(x_line_start), y:(y_line_start - 50)}};
+            line_g.push(list_temp);
+          }
+        }
+        else{//Truong hop diem ko thuoc cac group da tao
+          for(var i = 0; i < line_g.length; i++){
+            if(line_g[i]['group'] >= group_item){
+              group_item = group_item + 1;
+            }
+          }
+          var list_temp = {group:group_item,value:-1,coor:{x:(x_line_start), y:(y_line_start - 50)}};
+          line_g.push(list_temp);
+          var list_temp = {group:group_item,value:-1,coor:{x:(x_line_start), y:(y_line_start)}};
+          line_g.push(list_temp);
+        }
+      }
+      else {
+        var list_temp = {group:group_item,value:-1,coor:{x:(x_line_start), y:(y_line_start - 50)}};
+        line_g.push(list_temp);
+        var list_temp = {group:group_item,value:-1,coor:{x:(x_line_start), y:(y_line_start)}};
+        line_g.push(list_temp);
+      }
+      break;
+    }
+    case 4:
+    {
+      var group_item = 0;
+      if(line_g.length > 0){
+        y1 = y_line_start + 50;
+        y2 = y_line_start;
+        x = x_line_start;
+        var is_choose = false, y_choose = 0;
+        //Tim diem trung voi duong co san (group co san)
+        for(var i = 0; i < line_g.length; i++){
+          if(((line_g[i]['coor']['y'] == y1)||(line_g[i]['coor']['y'] == y2))&&(line_g[i]['coor']['x'] == x)){
+            group_item = line_g[i]['group'];
+            is_choose = true;
+            if(line_g[i]['coor']['y'] == y1){
+              y_choose = 1;
+            }
+            else{
+              y_choose = 2;
+            }
+            break;
+          }
+        }
+        if(is_choose){
+          if(y_choose == 1){
+            var list_temp = {group:group_item,value:-1,coor:{x:(x_line_start), y:(y_line_start)}};
+            line_g.push(list_temp);
+          }
+          else{
+            var list_temp = {group:group_item,value:-1,coor:{x:(x_line_start), y:(y_line_start + 50)}};
+            line_g.push(list_temp);
+          }
+        }
+        else{//Truong hop diem ko thuoc cac group da tao
+          for(var i = 0; i < line_g.length; i++){
+            if(line_g[i]['group'] >= group_item){
+              group_item = group_item + 1;
+            }
+          }
+          var list_temp = {group:group_item,value:-1,coor:{x:(x_line_start), y:(y_line_start + 50)}};
+          line_g.push(list_temp);
+          var list_temp = {group:group_item,value:-1,coor:{x:(x_line_start), y:(y_line_start)}};
+          line_g.push(list_temp);
+        }
+      }
+      else {
+        var list_temp = {group:group_item,value:-1,coor:{x:(x_line_start), y:(y_line_start + 50)}};
+        line_g.push(list_temp);
+        var list_temp = {group:group_item,value:-1,coor:{x:(x_line_start), y:(y_line_start)}};
+        line_g.push(list_temp);
+      }
+      break;
+    }
+  }
 }
 
 function press(e){
-  x_coor = e.x - x0;
-  y_coor = e.y;
-  var x_50 = Math.floor(x_coor/50);
-  var x_25 = Math.floor((x_coor%50));
-  var y_50 = Math.floor(y_coor/50);
-  var y_25 = Math.floor((y_coor%50));
-  var is_select = 0;
-  if(x_25 < 13){
-    var x_set = (x_50)*50;
-    is_select = is_select + 1;
-  }
-  if(x_25 > 37){
-    var x_set = (x_50 + 1)*50;
-    is_select = is_select + 1;
-  }
-  if(y_25 < 13){
-    var y_set = (y_50)*50;
-    is_select = is_select + 1;
-  }
-  if(y_25 > 37){
-    var y_set = (y_50 + 1)*50;
-    is_select = is_select + 1;
-  }
-  if(e.x > x0){
-    if(((x_25<13)||(x_25>37))&&((y_25<13)||(y_25>37))&&(!is_drapObject)){
-      is_addLine = true;
-      x_line_start = (x_50 + Math.floor(x_25/25))*50;
-      y_line_start = (y_50 + Math.floor(y_25/25))*50;;
+  console.log(e.target);
+  console.log(e.button);
+  if(e.button == 2){
+    for(var j = 0; j < battery_g.length; j++){
+      for(var i = 0; i < line_g.length; i++){
+        if((line_g[i]['coor']['x'] == (battery_g[j]['coor']['x'] - 25))&&(line_g[i]['coor']['y'] == battery_g[j]['coor']['y'])) {
+          value_line[line_g[i]['group']] = 1;
+        }
+        else{
+          if((line_g[i]['coor']['x'] == (battery_g[j]['coor']['x'] + 25))&&(line_g[i]['coor']['y'] == battery_g[j]['coor']['y'])){
+            value_line[line_g[i]['group']] = 2;
+          }
+        }
+      }
+    }
+
+    for(var i = 0; i < led_g.length; i++){
+      var pin1_value = -1, pin2_value = -1;
+      var coor1x = led_g[i]['coor']['x'] - 25;
+      var coor2x = led_g[i]['coor']['x'] + 25;
+      var coory = led_g[i]['coor']['y'];
+      for(var j = 0; j < line_g.length; j++){
+        if((coor1x == line_g[j]['coor']['x'])&&(coory == line_g[j]['coor']['y'])) {
+          pin1_value = value_line[line_g[j]['group']];
+        }
+        if((coor2x == line_g[j]['coor']['x'])&&(coory == line_g[j]['coor']['y'])) {
+          pin2_value = value_line[line_g[j]['group']];
+        }
+      }
+      var obj = document.getElementById('objectGroup').children[led_g[i]['index'] - 1];
+      if(((pin1_value == 1)&&(pin2_value == 2))||((pin1_value == 2)&&(pin2_value == 1))){
+        obj.setAttributeNS(xlinkns,"xlink:href", "#led_on");
+      }
     }
   }
-  //var x_set = (x_50 + x_25)*50;
-  //var y_set = (y_50 + y_25)*50;
+  else {
+    x_coor = e.x - x0;
+    y_coor = e.y;
+    var x_50 = Math.floor(x_coor/50);
+    var x_25 = Math.floor((x_coor%50));
+    var y_50 = Math.floor(y_coor/50);
+    var y_25 = Math.floor((y_coor%50));
+    var is_select = 0;
+    if(x_25 < 13){
+      var x_set = (x_50)*50;
+      is_select = is_select + 1;
+    }
+    if(x_25 > 37){
+      var x_set = (x_50 + 1)*50;
+      is_select = is_select + 1;
+    }
+    if(y_25 < 13){
+      var y_set = (y_50)*50;
+      is_select = is_select + 1;
+    }
+    if(y_25 > 37){
+      var y_set = (y_50 + 1)*50;
+      is_select = is_select + 1;
+    }
+    if(e.x > x0){
+      if(((x_25<13)||(x_25>37))&&((y_25<13)||(y_25>37))&&(!is_drapObject)){
+        is_addLine = true;
+        x_line_start = (x_50 + Math.floor(x_25/25))*50;
+        y_line_start = (y_50 + Math.floor(y_25/25))*50;;
+      }
+    }
+    //var x_set = (x_50 + x_25)*50;
+    //var y_set = (y_50 + y_25)*50;
 
-  if((is_select > 1)&&(x_set >= 50)&&(x_set <= 1550)&&(y_set >= 50)&&(y_set <= 750)){
-    //addPoint(x_set, y_set);
+    if((is_select > 1)&&(x_set >= 50)&&(x_set <= 1550)&&(y_set >= 50)&&(y_set <= 750)){
+      //addPoint(x_set, y_set);
 
 
+    }
   }
 }
 function startDrap_object(e){
@@ -269,7 +589,8 @@ function startDrap_object(e){
   if(((x_center > 15)&&(x_center < 35))){
     is_drapObject = true;
     target_move = e.target;
-    console.log(target_move);
+    //console.log(target_move);
+    document.body.style.cursor = 'grab';
   }
 
   x_coor = e.x - 7;
@@ -295,7 +616,7 @@ function startDrap_object(e){
     var y_set = (y_50 + 1)*50;
     is_select = is_select + 1;
   }
-  document.body.style.cursor = 'grab';
+
   //if(((x_25<13)||(x_25>37))&&((y_25<13)||(y_25>37))){
   //  is_addLine = true;
   //  x_line_start = (x_50 + Math.floor(x_25/25))*50;
@@ -379,18 +700,26 @@ function Drap_object(e){
       if(x_mouse > (x_line_start + 37)){
         addLine('line_horizontal',x_line_start + x0,y_line_start);
         x_line_start+=50;
+
+        addLine_G(1);
       }
       if(x_mouse < (x_line_start - 37)){
         x_line_start-=50;
         addLine('line_horizontal',x_line_start  + x0,y_line_start);
+
+        addLine_G(2);
       }
       if(y_mouse > (y_line_start + 37)){
         addLine('line_vertical',x_line_start  + x0,y_line_start);
         y_line_start+=50;
+
+        addLine_G(3);
       }
       if(y_mouse < (y_line_start - 37)){
         y_line_start-=50;
         addLine('line_vertical',x_line_start  + x0,y_line_start);
+
+        addLine_G(4);
       }
     }
     else{
@@ -445,9 +774,23 @@ function stopDrap_object(e){
   if(is_selecting){
     is_selecting = false;
 
+    var obj = document.getElementById('objectGroup');
     if(e.x < (x0 + 50)){
-      var obj = document.getElementById('objectGroup');
       obj.lastChild.remove();
+    }
+    if(led_g_status){
+      //console.log(obj.lastChild.getAttributeNS(null, 'x') - x0)
+      var list_temp = {index:index_object,coor:{x:(obj.lastChild.getAttributeNS(null, 'x') - x0), y:(obj.lastChild.getAttributeNS(null, 'y'))}};
+      console.log(list_temp)
+      led_g.push(list_temp)
+      led_g_status = false;
+    }
+    if(battery_g_status){
+      //console.log(obj.lastChild.getAttributeNS(null, 'x') - x0)
+      var list_temp = {index:index_object,coor:{x:(obj.lastChild.getAttributeNS(null, 'x') - x0), y:(obj.lastChild.getAttributeNS(null, 'y'))}};
+      console.log(list_temp)
+      battery_g.push(list_temp)
+      battery_g_status = false;
     }
   }
   document.body.style.cursor = 'crosshair';
@@ -470,8 +813,6 @@ function select_elements(e){
   is_selecting = true;
   document.body.style.cursor = 'grab';
 }
-
-
 
 function select_elements_touch(e){
   console.log(e.touches[0].pageX + '-' + e.touches[0].pageY);
@@ -548,15 +889,8 @@ $(document).ready(function() {
   document.getElementById('elementsGroup').addEventListener('touchstart',select_elements_touch,false);
   document.getElementById('svg_window').addEventListener('touchmove',drap_elements_touch,false);
 
+  document.addEventListener('contextmenu', event => event.preventDefault());
 
-  //document.getElementById('objectGroup').addEventListener('mousedown',startDrap_object,false);
-  //document.getElementById('objectGroup').addEventListener('mousemove',Drap_object,false);
-  //document.getElementById('objectGroup').addEventListener('mouseup',stopDrap_object,false);
-  //addled(125,100);
-  //addled(125,200);
-  //addbattery(225,200);
-  //addResistor(325,200);
-  //document.getElementById('body').addEventListener('onresize', resize_process, false);
   svg  = document.getElementById('svg_window');
   pt   = svg.createSVGPoint();
   dot  = document.querySelector('#dot');
